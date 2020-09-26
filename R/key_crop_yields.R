d7d479f26850348145ea44b30e844592c00665a3
Panagiotis Stylianos <pete.styl@gmail.com>
2020-09-26 14:30:46 +0200

    Add documentation for the dataset

---
+++ R/key_crop_yields.R
@@ -0,0 +1,9 @@
+#' Global key crop yields
+#'
+#' This dataset contains crop yields from 249 countries
+#'
+#' @source
+#' The data below comes from Our World in Data and sourced from Tidy Tuesday.
+#'
+
+"key_crop_yields"
