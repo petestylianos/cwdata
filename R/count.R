92c4782c5a02ac4e15802f1c3a7d23da87178ef2
Panagiotis Stylianos <pete.styl@gmail.com>
2020-10-12 01:07:34 +0300

    vig

--- R/count.R
+++ R/count.R
@@ -3,7 +3,9 @@
 #' @param data A dataset cotaining a country column
 
 #' @export
-
+#'
+#' @importFrom dplyr n_distinct
+#'
 n_countries <- function(data) {
-  dplyr::n_distinct(data$country)
+  n_distinct(data$country)
 }
